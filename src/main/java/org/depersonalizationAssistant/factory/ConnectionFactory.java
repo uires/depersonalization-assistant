648b5de87b859f9c264f0d83254c3fc2bba31280
uires <uiresdeivide1@gmail.com>
2018-07-24 13:24:16 -0300

    Adicionando classe factory para gerar conexão

---
+++ src/main/java/org/depersonalizationAssistant/factory/ConnectionFactory.java
@@ -0,0 +1,21 @@
+package org.depersonalizationAssistant.factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	private static final String URL = "jdbc:mysql://localhost:3306/depersonalization_assistant";
+	private static final String USER = "root";
+	private static final String PASSWORD = "root";
+
+	public static Connection getConnection() throws SQLException {
+		try {
+			Class.forName("org.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException error) {
+			error.printStackTrace();
+		}
+		return DriverManager.getConnection(URL, USER, PASSWORD);
+	}
+
+}
