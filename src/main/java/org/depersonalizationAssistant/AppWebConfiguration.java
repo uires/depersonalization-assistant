58c513e6b8bb5494394a62f6f38ae41218274aa8
uires <uiresdeivide1@gmail.com>
2018-07-24 12:47:18 -0300

    Adicionando classes de configuração do Spring

---
+++ src/main/java/org/depersonalizationAssistant/AppWebConfiguration.java
@@ -0,0 +1,19 @@
+package org.depersonalizationAssistant;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@EnableWebMvc
+@Configuration
+@ComponentScan(basePackageClasses = {})
+public class AppWebConfiguration {
+
+	@Bean
+	public InternalResourceViewResolver internalResourceViewResolver() {
+		return new InternalResourceViewResolver("WEB-INF/pages/", ".jsp");
+	}
+
+}
