75b5864caf1653a45721cd2ea7c75cc38595f03d
uires <uiresdeivide1@gmail.com>
2018-07-24 17:01:50 -0300

    injetando dependência no controllador

--- src/main/java/org/depersonalizationAssistant/controller/PacienteController.java
+++ src/main/java/org/depersonalizationAssistant/controller/PacienteController.java
@@ -1,23 +1,28 @@
 package org.depersonalizationAssistant.controller;
 
+import org.depersonalizationAssistant.dao.PacienteDAO;
 import org.depersonalizationAssistant.model.Endereco;
 import org.depersonalizationAssistant.model.Paciente;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class PacienteController {
+	@Autowired
+	private PacienteDAO dao;
 
 	@RequestMapping("paciente/cadastro")
 	public ModelAndView form() {
 		return new ModelAndView("paciente/cadastropaciente");
 	}
 
-	@RequestMapping("cadastrar")
+	@RequestMapping(value = "cadastrar", method = RequestMethod.POST)
 	public ModelAndView cadastro(Paciente paciente, Endereco endereco) {
 		paciente.setEndereco(endereco);
-		System.out.println(paciente);
+		dao.cadastraPaciente(paciente);
 		return new ModelAndView("/");
 	}
 }
