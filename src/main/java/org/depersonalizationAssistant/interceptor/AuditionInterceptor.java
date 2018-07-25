8f5e6b750fc88d314d34fddec86ea16e5ac11277
uires <uiresdeivide1@gmail.com>
2018-07-25 17:20:33 -0300

    adicionando preHandler para auditória de requisiçõe

---
+++ src/main/java/org/depersonalizationAssistant/interceptor/AuditionInterceptor.java
@@ -0,0 +1,31 @@
+package org.depersonalizationAssistant.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuditionInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+
+		if (request.getRequestURI().endsWith("logar") || request.getRequestURI().endsWith("login")
+				|| request.getRequestURI().endsWith("sobre") || request.getRequestURI().contains("resources")) {
+			return true;
+		}
+		if((request.getSession().getAttribute("usuario.logado")!= null) && request.getRequestURI().endsWith("login")){
+			response.sendRedirect("/dashboard");
+		}
+		if (request.getSession().getAttribute("usuario.logado") != null) {
+			return true;
+		} else {
+			response.sendRedirect("paciente/login");
+			
+			return false;
+		}
+
+	}
+
+}
