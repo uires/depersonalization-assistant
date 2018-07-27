490ebfa4a986746fbadb72c3f36f9525699bb1f1
uires <uiresdeivide1@gmail.com>
2018-07-26 21:52:26 -0300

    adicionando método para mapear requisição e de cadastrar relatorio

--- src/main/java/org/depersonalizationAssistant/controller/RelatorioController.java
+++ src/main/java/org/depersonalizationAssistant/controller/RelatorioController.java
@@ -1,8 +1,26 @@
 package org.depersonalizationAssistant.controller;
 
+import org.depersonalizationAssistant.model.Patologia;
+import org.depersonalizationAssistant.model.Relatorio;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RelatorioController {
-	
+
+	@RequestMapping("relatorio/cadastrar")
+	public ModelAndView cadastrarMap() {
+		return new ModelAndView("relatorio/cadastrarelatorio");
+	}
+
+	@RequestMapping(value = "cadastrar", method = RequestMethod.POST)
+	public ModelAndView cadastrarRelatorio(Relatorio relatorio, Patologia patologia) {
+		relatorio.setPatologia(patologia);
+		System.out.println(relatorio.getDescricao());
+		System.out.println(patologia.getDataInicio());
+		return new ModelAndView("redirect:/dashboard");
+	}
+
 }
