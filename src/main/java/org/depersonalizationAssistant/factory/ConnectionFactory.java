22ed435f97d79bc2fc0614abf05174fc621f18d2
uires <uiresdeivide1@gmail.com>
2018-07-28 14:24:25 -0300

    complementando constante URL

--- src/main/java/org/depersonalizationAssistant/factory/ConnectionFactory.java
+++ src/main/java/org/depersonalizationAssistant/factory/ConnectionFactory.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
-	private static final String URL = "jdbc:mysql://localhost:3306/depersonalization_assistant";
+	private static final String URL = "jdbc:mysql://localhost:3306/depersonalization_assistant?autoReconnect=true&useSSL=false";
 	private static final String USER = "root";
 	private static final String PASSWORD = "root";
 
